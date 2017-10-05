b7b89f79d71403cddeda1900f439103217b3202a
Metzing Márton <marton.metzing@gmail.com>
2017-10-05 22:00:40 +0200

    Model
    Added model class and _database_

---
+++ SWH/Model/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWH.Model
+{
+    class User
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PlaceOfBirth { get; set; }
+        public string HomeTown { get; set; }
+    }
+}
